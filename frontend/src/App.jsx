d4b67593858b14d7e751891c05f528e520c5ef80
Himanshu Rane <himanshurane6501@gmail.com>
2024-09-24 11:38:47 +0530

    Vite config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,12 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import Layout from "./components/layout/Layout";
-
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/auth/LoginPage";
-import SignUpPage from "./pages/auth/SignUpPage";
-import toast, { Toaster } from "react-hot-toast";
-import { useQuery } from "@tanstack/react-query";
-import { axiosInstance } from "./lib/axios";
-import NotificationsPage from "./pages/NotificationsPage";
-import NetworkPage from "./pages/NetworkPage";
-import PostPage from "./pages/PostPage";
-import ProfilePage from "./pages/ProfilePage";
-import ActivationPage from "./pages/ActivationPage";
+import { lazy, Suspense } from "react";
+const HomePage = lazy(() => import("./pages/HomePage"));
+const LoginPage = lazy(() => import("./pages/auth/LoginPage"));
+const SignUpPage = lazy(() => import("./pages/auth/SignUpPage"));
+const NotificationsPage = lazy(() => import("./pages/NotificationsPage"));
+const NetworkPage = lazy(() => import("./pages/NetworkPage"));
+const PostPage = lazy(() => import("./pages/PostPage"));
+const ProfilePage = lazy(() => import("./pages/ProfilePage"));
+const ActivationPage = lazy(() => import("./pages/ActivationPage"));
 
 function App() {
   const { data: authUser, isLoading } = useQuery({
@@ -24,48 +19,51 @@ function App() {
         if (err.response && err.response.status === 401) {
           return null;
         }
-        toast.error(err.response.data.message || "Something went wrong");
+        toast.error(err.response?.data?.message || "Something went wrong");
+        return null;
       }
     },
   });
 
-  if (isLoading) return null;
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <Layout>
-      <Routes>
-        <Route
-          path="/"
-          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
-        />
-        <Route path="/activate" element={<ActivationPage />} />
-        <Route
-          path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
-        />
-        <Route
-          path="/notifications"
-          element={
-            authUser ? <NotificationsPage /> : <Navigate to={"/login"} />
-          }
-        />
-        <Route
-          path="/network"
-          element={authUser ? <NetworkPage /> : <Navigate to={"/login"} />}
-        />
-        <Route
-          path="/post/:postId"
-          element={authUser ? <PostPage /> : <Navigate to={"/login"} />}
-        />
-        <Route
-          path="/profile/:username"
-          element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />}
-        />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
+          <Route path="/activate" element={<ActivationPage />} />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/notifications"
+            element={
+              authUser ? <NotificationsPage /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/network"
+            element={authUser ? <NetworkPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/post/:postId"
+            element={authUser ? <PostPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/profile/:username"
+            element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </Suspense>
       <Toaster />
     </Layout>
   );
